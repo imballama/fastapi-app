3c3c6aab1efcec5f3b38607c49a96986b6d5ba48
imballama <Inkvizzzz@gmail.com>
2023-06-05 16:03:50 +0300

    implement get request for job along with test

--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -3,9 +3,14 @@ from schemas.jobs import JobCreate
 from db.models.jobs import Job
 
 
-def create_new_job(job: JobCreate, db: Session, owner_id:int):
+def create_new_job(job: JobCreate, db: Session, owner_id: int):
     job_object = Job(**job.dict(), owner_id=owner_id)
     db.add(job_object)
     db.commit()
     db.refresh(job_object)
     return job_object
+
+
+def retrieve_jobs(id: int, db: Session):
+    item = db.query(Job).filter(Job.id == id).first()  # select * from job where id = 1;
+    return item
